13bacc25e23be8ea8ce22424fe2b90295661caf0
Alex Shapran <shapran400@gmail.com>
2017-04-13 01:57:08 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -13,17 +13,7 @@ int main()
 	unsigned int size = 0;
 	auto temp = 0;
 
-	cout << "Print the number of nodes: ";
 	cin >> temp;
-	try
-	{
-		if(temp < 0) throw 1;
-	}
-	catch(int test)
-	{
-		cout << endl << "[-] Exception " << test << ": The number of nodes can't be negative!!!" << endl;
-		return 0;
-	}
 	size = temp;
 
 	node = new unsigned int [size];
@@ -32,20 +22,8 @@ int main()
 	{
 		cout << "Add node " << i+1 << ": ";
 		cin >> temp;
-		try
-		{
-			if( temp < 0 ) throw 2;
-		}
-		catch( int test )
-		{
-			cout << endl << "[-] Exception " << test << ": The node of tree can't be negative!!!" << endl;
-			return 0;
-		}
 		node[i] = temp;
-		cout << endl;
 	}
-
-
 	for (unsigned int i = 0; i < size; ++i)
 	{
 		ptr->Add(node[i], root);
@@ -53,14 +31,11 @@ int main()
  
 	ptr->print(root);
 	ptr->Search(root);
-	if( n == 0 ) cout << endl << "[-] The number " << number << " was not found!:(";
 
 	ptr->Deletion(root);
-	cout << endl << "The result of deletion: " << endl;
 	ptr->print(root);
 
 	ptr->FreeTree(root);
  	delete [] node;
-	cout << endl << endl << "Press any button to exit the program " << endl;
 	return 0;
 }
