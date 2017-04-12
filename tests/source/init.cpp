aa4da0ff75e3493a305696665d81b221d80f0a50
Alex Shapran <shapran400@gmail.com>
2017-04-12 23:35:09 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -3,9 +3,9 @@
 
 SCENARIO("Add", "[Add]") {
     int x = 10;
-    BinarySearchTree *r = 0;
+   // BinarySearchTree *r = 0;
     
-    void *rv = BinarySearchTree::r->Add( x, r );
+    void *rv = BinarySearchTree::root->Add( x, root );
     
     REQUIRE( rv == nullptr);
 }
