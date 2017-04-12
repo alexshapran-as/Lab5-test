aad08a21bf6d9aa4ede7e9be3893acde2f5cd4ee
Alex Shapran <shapran400@gmail.com>
2017-04-13 02:03:35 +0300

    Update Lab5-test.hpp

--- include/Lab5-test.hpp
+++ include/Lab5-test.hpp
@@ -3,7 +3,7 @@
 using namespace std;
  
 int tabs = 0;
-int number = 0;
+int number;
 unsigned int k = 0;
 unsigned int n = 0;
 
@@ -19,10 +19,10 @@ public:
 	unsigned int data;
 	BinarySearchTree *r;
 	void Add(unsigned int new_data, BinarySearchTree *&root);
-	void Search(BinarySearchTree *root);
-	void print(BinarySearchTree *root);
-	void Deletion(BinarySearchTree *root);
-	BinarySearchTree* findMaxNode(BinarySearchTree *root);
-	void removeNode(BinarySearchTree *root, int number, BinarySearchTree *parent);
-	void FreeTree(BinarySearchTree *&root);
+	//void Search(BinarySearchTree *root);
+	//void print(BinarySearchTree *root);
+	//void Deletion(BinarySearchTree *root);
+	//BinarySearchTree* findMaxNode(BinarySearchTree *root);
+	//void removeNode(BinarySearchTree *root, int number, BinarySearchTree *parent);
+	//void FreeTree(BinarySearchTree *&root);
 };
