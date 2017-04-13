17886002759e88d2218cf726ee3ed04c24f75723
Alex Shapran <shapran400@gmail.com>
2017-04-13 22:30:48 +0300

    Update Lab5-test.hpp

--- include/Lab5-test.hpp
+++ include/Lab5-test.hpp
@@ -14,7 +14,7 @@ public:
 	int Add(unsigned int new_data, BinarySearchTree *&root);
 	int print(int check1, BinarySearchTree *root);
 	int Search(int check2, BinarySearchTree *root);
-	//void Deletion(BinarySearchTree *root);
+	int Deletion(int check3, BinarySearchTree *root);
 	//BinarySearchTree* findMaxNode(BinarySearchTree *root);
 	//void removeNode(BinarySearchTree *root, int number, BinarySearchTree *parent);
 	//void FreeTree(BinarySearchTree *&root);
